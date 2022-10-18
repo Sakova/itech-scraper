3099715f94e4637f6cb2e043175d2ff74c8326b2
Alexander Korchyk <aliaksandr.korchyk@gmail.com>
2022-10-18 17:16:37 +0300

    feat: set up views for article

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,8 @@ class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    if current_user
+      @articles = current_user.articles
+    end
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ApplicationRecord
   belongs_to :user
+  validates :link, presence: true
 end
