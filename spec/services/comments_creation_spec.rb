220c3d0a4a2e3049ceb4de255c6d3cf69638a1ca
Alexander Korchyk <aliaksandr.korchyk@gmail.com>
2022-10-25 18:34:52 +0300

    feat: create specs for model, controller, service and gems for testing

---
+++ spec/services/comments_creation_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe 'CommentsCreation' do
+  let(:user) do
+    User.create(email: 'test@example.com', password: '123456',
+                password_confirmation: '123456')
+  end
+
+  let(:article) do
+    Article.create(title: 'Test', link: 'link.com', rating: nil, user_id: user.id)
+  end
+
+  before do
+    sign_in user
+  end
+
+  context 'with correct data' do
+    it 'creates new comments' do
+      expect{
+        CommentsCreation.new(['Лучший коммент в мире'], ['The best comment in the world'], article.id).rating
+      }.to change(Comment, :count).by(1)
+    end
+
+    it 'adds rating to the article' do
+      CommentsCreation.new(['Лучший коммент в мире'], ['The best comment in the world'], article.id).rating
+      expect(Article.find(comment.article_id).rating).not_to be_nil
+    end
+  end
+end
